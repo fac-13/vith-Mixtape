9f1f80c7a6fca595cebb8b3299711941f3d752bf
ybird21 <tomgdoran@gmail.com>
2018-03-14 15:16:15 +0000

    succesfully linked dummy file and test file Relates #2

--- logic.test.js
+++ logic.test.js
@@ -1,6 +1,8 @@
 const test = require("tape");
 const logic = require("./logic");
-const dummy = require("./logic.data.dummy");
+const dummies = require("./logic.data.dummy");
+const giphyDummy = dummies.giphyDummy;
+const musicDummy = dummies.musicDummy;
 
 test("Testing Tape is working", function(t) {
     t.equal(1, 1, "One should equal one");
@@ -14,7 +16,13 @@ test("Testing Tape is working", function(t) {
 // -- API RESPONSE FUNCTIONS --
 
 // Giphy code
-
+test("Testing giphyDummy is imported", function(t) {
+    if (typeof giphyDummy === "object"){
+        t.pass("giphyDummy is imported");
+    }
+    else{t.fail("giphyDummy is not imported")};
+    t.end();
+});
 // giphyFunction.[function name]
 
 
