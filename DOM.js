7593e5a6bb20c0f9822057efdd7dacc4d82591f9
Helen Zhou <helen.zhou6@gmail.com>
2018-03-14 13:16:29 +0000

    More first commit

--- DOM.js
+++ DOM.js
@@ -1,30 +1,32 @@
-var gifResult = ''; //this will have the URL of the gif 
-var musicResult = [];  // have object with the 10 titles and artist names
-var playlistTitle = '';
+(function () {
+  var gifResult = ''; //this will have the URL of the gif 
+  var musicResult = [];  // have object with the 10 titles and artist names
+  var playlistTitle = '';
 
-// -- DOM / REQUEST FUNCTIONS --
+  // -- DOM / REQUEST FUNCTIONS --
 
-// Ties in functions from logic into here
+  // Ties in functions from logic into here
 
-addListener('#js-submit', 'submit', function(event){
-  // Add in functions that generates the correct URL
-  playlistTitle = event.target[0].value;
-  // use playlistTitle to adjust the URL to what needed
-  var UrlGiphy = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
-  var UrlMusic = "";
+  logicFunctions.addListener('#js-submit', 'submit', function (event) {
+    // Add in functions that generates the correct URL
+    playlistTitle = event.target[0].value;
+    // use playlistTitle to adjust the URL to what needed
+    var UrlGiphy = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+    var UrlMusic = "";
 
-  gifResult = fetch(UrlGiphy, selectGif);
-  musicResult = fetch(UrlGiphy, selectMusic);
+    gifResult = logicFunctions.fetch(UrlGiphy, selectGif);
+    musicResult = logicFunctions.fetch(UrlGiphy, selectMusic);
 
-})
+  })
 
 
 
-// -- DOM MANIPULATE FUNCTIONS --
-// Use code to adjust the empty h2 with the playlist name using var playlistTitle
+  // -- DOM MANIPULATE FUNCTIONS --
+  // Use code to adjust the empty h2 with the playlist name using var playlistTitle
 
-// Use gifResult to manipulate DOM
+  // Use gifResult to manipulate DOM
 
-// Use musicResult to manipulate DOM
+  // Use musicResult to manipulate DOM
 
-// Once DOM manipulated: reset (on page reload)??
+  // Once DOM manipulated: reset (on page reload)??
+})();
--- logic.test.js
+++ logic.test.js
@@ -1,8 +1,23 @@
+var test = require("tape");
+var logic = require("./logic");
+// var dummy = request("./logic.data.dummy");
+
+test("Testing Tape is working", function(t) {
+    t.equal(1, 1, "One should equal one");
+    t.end();
+});
+
 // -- DOM / REQUEST FUNCTIONS --
 
+// DOMFunctions.[function name]
 
 // -- API RESPONSE FUNCTIONS --
 
 // Giphy code
 
+// giphyFunction.[function name]
+
+
 // Music code
+
+// musicFunctions.[function name]
