bcab8bee9df0daf5b21fdfb8df3c6d7e7d2de7da
vlbee <32312712+vlbee@users.noreply.github.com>
2018-03-15 15:55:12 +0000

    playlist title displays relates #21 but not complete yet"

--- DOM.js
+++ DOM.js
@@ -11,10 +11,15 @@
 	logic.addListener('#js-submit', 'submit', function(event) {
 		event.preventDefault();
 		var input = event.target[0].value; // use input to adjust the URL to what needed
-    
-    	// URLs and Data Fetching
-		var urlGiphy = 'http://api.giphy.com/v1/gifs/search?q=' + input + '&api_key=' + giphyKey;
-		var urlMusic = 'https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=' + input + '&f_lyrics_language=en&f_has_lyrics=1&s_artist_rating=asc&s_track_rating=asc&quorum_factor=1&page_size=20&apikey=' + musicKey; // uses cors-anywhere proxy to prevent CORS errors
+
+		// URLs and Data Fetching
+		var urlGiphy =
+			'http://api.giphy.com/v1/gifs/search?q=' + input + '&api_key=' + giphyKey;
+		var urlMusic =
+			'https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=' +
+			input +
+			'&f_lyrics_language=en&f_has_lyrics=1&s_artist_rating=asc&s_track_rating=asc&quorum_factor=1&page_size=20&apikey=' +
+			musicKey; // uses cors-anywhere proxy to prevent CORS errors
 
 		//TITLE
 		playlistTitle = input;
@@ -23,8 +28,10 @@
 		logic.fetch(urlGiphy, logic.selectGif, displayGif);
 
 		//MUSIC
-    	logic.fetch(urlMusic, logic.selectMusic, displayMusic);
-    
+		logic.fetch(urlMusic, logic.selectMusic, displayMusic);
+
+		//DOES WHAT IS SAYS
+		displayTitle(playlistTitle);
 	});
 
 	// -- DOM MANIPULATE FUNCTIONS --
@@ -35,32 +42,41 @@
 		gifResult = gif;
 		var img = document.querySelector('#js-gif');
 		img.src = gifResult;
+		img.classList.remove('main_hidden');
 	}
 
 	// Use musicResult to manipulate DOM
 	function displayMusic(music) {
 		musicResult = music;
-		var tracklist = document.querySelector('#js-tracklist');
 
 		// refreshes tracklist for repeadted searches
+		var tracklist = document.querySelector('#js-tracklist');
 		while (tracklist.firstChild) {
-			tracklist.removeChild(tracklist.firstChild)
+			tracklist.removeChild(tracklist.firstChild);
 		}
 
 		musicResult.forEach(function(trackObj) {
 			var track = document.createElement('li');
-			
+
 			var title = document.createElement('span');
-			title.textContent =  trackObj.track;
+			title.textContent = trackObj.track;
 			track.appendChild(title);
 
 			var artist = document.createElement('span');
-			artist.textContent =  trackObj.artist;
+			artist.textContent = trackObj.artist;
 			track.appendChild(artist);
 
 			tracklist.appendChild(track);
-		})
-	};
-  // Once DOM manipulated: reset (on page reload)??
-  
+		});
+	}
+
+	function displayTitle(title) {
+		var titleDiv = document.querySelector('#js-title');
+		document.querySelector('#js-title');
+		var mixTapeTitle = document.querySelector('#js-playlist-name');
+		mixTapeTitle.textContent = playlistTitle;
+		titleDiv.classList.remove('main_hidden');
+	}
+	//STRETCH GOAL!!!!!!!
+	// Once DOM manipulated: reset (on page reload)??
 })();
